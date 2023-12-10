ea65b62b4b12bbbb7496e84e80ebe976bc93c255
Alex <dev.alexmorales@gmail.com>
2023-12-09 22:36:29 -0300

    CriarUsuario e Validação dos Formulários

--- src/controllers/logincontroller.js
+++ src/controllers/logincontroller.js
@@ -1,4 +1,31 @@
+const Login = require('../Models/LoginModel');
+
 module.exports.index = (req, res, next) => {
-    console.log('Passei pelo index do login')
     res.render('login');
-}
+};
+
+module.exports.register = async (req, res, next) => {
+    const login = new Login(req.body);
+   
+    try{
+        await login.register();
+
+        if(login.errors.length > 0){
+            req.flash('errors', login.errors);
+            req.session.save(() => {
+                res.redirect('back');
+                return;
+            });
+            return;
+        };
+
+        req.flash('success', 'Seu usuário foi criado com sucesso!');
+        req.session.save(() => {
+            res.redirect('back');
+            return;
+        });
+    }catch(err){
+        console.log(err);
+        res.rend('404');
+    };
+};
